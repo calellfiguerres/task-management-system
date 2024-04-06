929917e21c78a032db955fa2130419331576d4fe
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2024-04-06 16:58:29 -0700

    Added EJS to Express and sample view, static file

--- src/Controller/routes/index.ts
+++ src/Controller/routes/index.ts
@@ -3,7 +3,7 @@ import express, { Router, Request, Response } from "express";
 const router: Router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-    res.send("Hello world! How are you?");
+    res.render("index", { test: "hello world!" })
 });
 
 module.exports = router;
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ const app: Express = express();
 
 const indexRouter = require("./Controller/routes/index");
 
+app.set("view engine", "ejs");
+app.set("views", "./src/Views")
+
 app.use("/static", express.static("./src/Views/static"))
 app.use("/", indexRouter);
 
