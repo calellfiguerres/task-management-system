4884cfcc43775f2691e50cc84d939c87f88bbdc3
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2024-04-06 20:42:25 -0700

    Add some comments and `body-parser` for form processing

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { Express } from "express";
 
-// const indexRouter = require()
-
+// Get the Express app we create
 const app: Express = require("./src/app");
 const port = 3000;
 
+// Run it on the specified port
 app.listen(port, () => {
     console.log(`[Server]: Server is listening to http://localhost:${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,26 @@
 import express, { Express } from "express";
+const bodyParser = require("body-parser");
 
+// Create the Express app
 const app: Express = express();
 
-const indexRouter = require("./Controller/routes/index");
-
+// Set the view engine to EJS and which directory it search for templates
 app.set("view engine", "ejs");
 app.set("views", "./src/Views")
 
+// Body parser for forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set the static directory to use
 app.use("/static", express.static("./src/Views/static"))
+
+// Load the `index` routes
+const indexRouter = require("./Controller/routes/index");
 app.use("/", indexRouter);
 
+// Load the `auth` routes
+const authRouter = require("./Controller/routes/auth");
+app.use("/", authRouter)
+
+// Export Express app
 module.exports = app;
