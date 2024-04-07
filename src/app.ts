f42315455b5053be5625c74cbaeb6e48bbae49f4
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2024-04-07 01:09:42 -0700

    Add session handling using `expression-session` and `passportjs` dependency

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 // Create the Express app
 const app: Express = express();
@@ -7,6 +8,14 @@ const app: Express = express();
 // Set the view engine to EJS and which directory it search for templates
 app.set("view engine", "ejs");
 app.set("views", "./src/Views")
+
+// Session handling
+app.use(session({
+    secret: "this is a very secret thing",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 
 // Body parser for forms
 app.use(bodyParser.urlencoded({ extended: true }));
