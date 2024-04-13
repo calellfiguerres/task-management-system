3538259707259a8017897e155d4ec0ae813e44ef
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2024-04-13 13:56:18 -0700

    Added some common redirects on the index page

--- src/Controller/routes/index.ts
+++ src/Controller/routes/index.ts
@@ -6,4 +6,12 @@ router.get("/", (req: Request, res: Response) => {
     res.render("index", { test: "hello world!" })
 });
 
+router.get("/login", (req: Request, res: Response) => {
+    res.redirect("/auth/login");
+});
+
+router.get("/register", (req: Request, res: Response) => {
+    res.redirect("/auth/register");
+});
+
 module.exports = router;
