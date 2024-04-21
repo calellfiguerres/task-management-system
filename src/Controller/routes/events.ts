afb19f6f10068bffefb6f156a22628a2c98076d4
AlexanderLarsen343 <alexander.m.larsen@wsu.edu>
2024-04-20 17:47:59 -0700

    Created Events basic routing. Also added the "viewevents" page in View

--- src/Controller/routes/events.ts
+++ src/Controller/routes/events.ts
@@ -1,10 +1,34 @@
 import express, { Request, Response, Router } from "express";
 import passport from "passport";
+import { User } from "../../Models/User";
+import { Event } from "../../Models/Event";
 
 const router: Router = express.Router();
 
 router.get("/", passport.authenticate("session"), async (req: Request, res: Response) => {
-    res.send("asdf");
+    
+    if (!req.isAuthenticated()) { // Someone not authenticated and trying to skip login
+        res.redirect('/auth/login');
+        return;
+    }
+    if (!(req.user instanceof User)) {
+        res.status(500).send("error");
+        return;
+    }
+
+    const user: User = req.user;
+
+    const events: Event[] = await Event.findAll({
+        where: {
+            eventOwnerId: req.user.id
+        },
+    });
+
+    res.render('events/viewevents', {
+        events: events,
+        isAuthenticated: req.isAuthenticated(),
+        user: req.user
+    });
 });
 
 module.exports = router;
