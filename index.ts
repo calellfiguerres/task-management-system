1791d512d5459726aae3b4b2deb5d0258c4cbed2
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2024-04-21 19:20:32 -0700

    App runner is now based on environment settings

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import { Express } from "express";
 
 // Get the Express app we create
 const app: Express = require("./src/app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Run it on the specified port
 app.listen(port, () => {
-    console.log(`[Server]: Server is listening to http://localhost:${port}`);
+    console.log(`[Server]: Server is listening to http://${process.env.HOSTNAME}:${port}`);
 });
